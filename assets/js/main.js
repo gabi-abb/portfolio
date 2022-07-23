43a088a2219ec698a3011949be18275be46cc511
gabi-abb <76527384+gabi-abb@users.noreply.github.com>
2022-07-23 14:23:28 +0300

    111

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function copyToClipboard() {
+	/* Get the text field */
+	var copyText = document.getElementById("myInput");
+  
+	/* Select the text field */
+	copyText.select();
+	copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+	 /* Copy the text inside the text field */
+	navigator.clipboard.writeText(copyText.value);
+  
+	/* Alert the copied text */
+	alert("Copied the text: " + copyText.value);
+  }
+
 (function($) {
 
 	var	$window = $(window),
